837280eccb9cb2bea37a7cee5eb3c7a9e12debfb
David Dickinson <davrdic@gmail.com>
2025-04-05 12:47:19 -0500

    adding domino class and connecting to database. full connection from fe to db on local.

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import json
 import os
+import datetime
+import pprint
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from typing import Union
 from pydantic import BaseModel
+from starlette.requests import Request
 
-client = MongoClient(os.getenv("DATABASE_CONNECTION_STRING"))
 
-try:
-    client.admin.command('ping')
-    print("Connected to MongoDB successfully!")
-except Exception as e:
-    print(f"An error occurred: {e}")
+class Domino:
+    def __init__(self, sida_a, side_b):
+        self.side_a = sida_a
+        self.side_b = side_b
 
-client.close()
+domino = Domino(1, 2)
+
+class GameInfo(BaseModel):
+    player_one_hand : str
+
+# client = MongoClient(os.getenv("DATABASE_CONNECTION_STRING"))
+#
+# try:
+#     client.admin.command('ping')
+#     db = client.BookStore
+#     collection = db.Books
+#     post = {
+#         "author": "Mike",
+#         "text": "My first blog post!",
+#         "tags": ["mongodb", "python", "pymongo"],
+#         "date": datetime.datetime.now(tz=datetime.timezone.utc)
+#     }
+#     posts = db.posts
+#     post_id = posts.insert_one(post).inserted_id
+#     pprint.pprint(posts.find_one())
+#     print("Connected to MongoDB successfully!")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
+#
+# client.close()
 
 from fastapi import FastAPI
 
@@ -27,12 +52,61 @@ def home():
 @app.get("/game_state")
 def game_state():
     x = {
-      "sideA": 1,
-      "sideB": 1
+        'd_one': {
+            "side_a": 1,
+            "side_b": 1
+        },
+        'd_two': {
+            "side_a": 2,
+            "side_b": 2
+        }
     }
+    #anotherx = '{"d_one": {"side_a": 1, "side_b": 1}, "d_two": {"side_a": 2, "side_b": 2}}'
     xstring = json.dumps(x)
+    #print("xstring: " + xstring)
     return (
 
             {"date": "2025-04-04", "temperatureC": 51, "temperatureF": 123, "summary": "Freezing", "domino": xstring}
-
     )
+
+@app.post("/create_game")
+async def create_game(request: Request):
+    print("Success")
+    data = await request.json()
+    print(data)
+    client = MongoClient(os.getenv("DATABASE_CONNECTION_STRING"))
+    try:
+        db = client.BookStore
+        posts = db.posts
+        post_id = posts.insert_one(data).inserted_id
+        print(post_id)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return
+
+@app.post("/create_game_bad")
+def create_game_bad(game_info : GameInfo):
+    client = MongoClient(os.getenv("DATABASE_CONNECTION_STRING"))
+
+    try:
+        client.admin.command('ping')
+        db = client.BookStore
+        collection = db.Books
+        # post = {
+        #     "author": "Mike",
+        #     "text": "My first blog post!",
+        #     "tags": ["mongodb", "python", "pymongo"],
+        #     "date": datetime.datetime.now(tz=datetime.timezone.utc)
+        # }
+        posts = db.posts
+        #str_result = str(GameInfo)
+        #print("str_result" + str_result)
+        #print("str_result: " + str_result)
+        #post_id = posts.insert_one(str(GameInfo)).inserted_id
+        #pprint.pprint(posts.find_one())
+        print("Connected to MongoDB successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    client.close()
+    return game_info
